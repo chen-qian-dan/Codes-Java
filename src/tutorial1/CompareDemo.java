1af189783a78a9ea222dbe014254e164c322f886
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-12-09 07:49:13 +1030

    Compare integers;

---
+++ src/tutorial1/CompareDemo.java
@@ -0,0 +1,39 @@
+package tutorial1;
+
+public class CompareDemo {
+	public static void main(String[] args) {
+		
+		int x = 6; 
+		int y = 7; 
+		int z = 10; 
+		// > < == >= <= !=
+		
+		boolean compare = x > y; 	
+		System.out.println(compare); 
+		
+		compare = x >= y; 
+		System.out.println(compare); 
+     
+		compare = x < y; 
+		System.out.println(compare); 
+		
+		compare = x <= y; 
+		System.out.println(compare); 
+		
+		compare = x == y; 
+		System.out.println(compare); 
+		
+		compare = x != y; 
+		System.out.println(compare); 
+	}
+
+}
+
+
+
+
+
+
+
+
+
