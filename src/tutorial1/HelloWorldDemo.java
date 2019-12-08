cfd39f4440d2ce72e73a6c6a547bbd3812ad68f2
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-12-08 22:25:49 +1030

    Print "Hello World!" on console;

---
+++ src/tutorial1/HelloWorldDemo.java
@@ -0,0 +1,11 @@
+package tutorial1;
+
+public class HelloWorldDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!"); 
+
+	}
+
+}
