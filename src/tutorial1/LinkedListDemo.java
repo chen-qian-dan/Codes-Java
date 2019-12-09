f8106cdeffd39e28cd8e12c55ed7f8fd70ac7b45
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-12-09 16:59:38 +1030

    LinkedListDemo

---
+++ src/tutorial1/LinkedListDemo.java
@@ -0,0 +1,24 @@
+package tutorial1;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> t = new LinkedList<Integer>(); 
+		t.add(0); 
+		t.add(1);
+		t.add(2); 
+		t.add(3); 
+		t.add(4); 
+		t.add(5); 
+		t.add(6); 
+		t.add(7); 
+		t.add(8); 	
+		
+		System.out.println(t); 
+
+	}
+
+}
