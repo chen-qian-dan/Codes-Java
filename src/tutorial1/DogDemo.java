71ed8c461e2bff79d9e37ee369e270f3baf591f3
chen-qian-dan <54783217+chen-qian-dan@users.noreply.github.com>
2019-12-10 20:45:36 +1030

    Add sub Class BlackDog.

--- src/tutorial1/DogDemo.java
+++ src/tutorial1/DogDemo.java
@@ -5,6 +5,11 @@ public class DogDemo {
 	public static void main(String[] args) {
 		Dog Dog1 = new Dog("Qian", 12); 
 		Dog1.speak();
+		
+		
+		
+		Dog oBlackDog = new BlackDog("Chen", 10, "big"); 
+		oBlackDog.speak();
 
 	}
 
